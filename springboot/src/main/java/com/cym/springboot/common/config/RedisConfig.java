f2c48b2070fa82c39e3ef37c880c3dd0e293c6dd
lzjtu-cym <1601561783@qq.com>
2021-01-27 14:55:19 +0800

    redis工具类添加

---
+++ springboot/src/main/java/com/cym/springboot/common/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.cym.springboot.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @Description: Redis配置类
+ * @Date: 2021/1/27 14:40
+ * @Author: lyf
+ * @Version: 1.0
+ */
+
+@Configuration
+public class RedisConfig {
+
+    //redisTemplate注入到Spring容器
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        redisTemplate.setConnectionFactory(factory);
+        //key序列化
+        redisTemplate.setKeySerializer(redisSerializer);
+        //value序列化
+        redisTemplate.setValueSerializer(redisSerializer);
+        //value hashmap序列化
+        redisTemplate.setHashKeySerializer(redisSerializer);
+        //key hashmap序列化
+        redisTemplate.setHashValueSerializer(redisSerializer);
+        return redisTemplate;
+    }
+}
